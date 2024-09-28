03301d8dcf052a91e711ffb7f2d6c0711f177c7c
Ayush720-sudo <145530241+Ayush720-sudo@users.noreply.github.com>
2024-09-28 19:16:28 +0200

    DAY1

---
+++ DAY1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X;// X denoting the total points scored by given team
+	cin>>X;
+	if(X>=12){
+	    cout<<"YES";
+	}
+	else cout<<"NO";
+	return 0;
+}
